6302e9e3b186dea6cc0920f45f57f42150791f23
Jannik H. Orzek <jannik.orzek@mailbox.org>
2024-11-22 09:37:26 +0100

    added basic gt support

---
+++ R/to_gt.R
@@ -0,0 +1,234 @@
+##### Functions #####
+#' to_gt
+#'
+#' Translates a table created with tablespan to a great table (gt). See https://gt.rstudio.com/.
+#'
+#' Tablespan itself does not provide any printing of tables as HTML table. However,
+#' with to_gt, tablespan can be translated to a great table which provides html and
+#' LaTeX output.
+#'
+#' @param tbl table created with tablespan::tablespan
+#' @param groupname_col Provide column names to group data. See ?gt::gt for more
+#' details.
+#' @param ... additional arguments passed to gt::gt().
+#' @import gt
+#' @export
+#' @examples
+#' library(tablespan)
+#' library(dplyr)
+#' data("mtcars")
+#'
+#' summarized_table <- mtcars |>
+#'   group_by(cyl, vs) |>
+#'   summarise(N = n(),
+#'             mean_hp = mean(hp),
+#'             sd_hp = sd(hp),
+#'             mean_wt = mean(wt),
+#'             sd_wt = sd(wt))
+#'
+#' tbl <- tablespan(data = summarized_table,
+#'                  formula = (LHS = Cylinder:cyl + Engine:vs) ~
+#'                    N +
+#'                    (Results = (`Horse Power` = Mean:mean_hp + SD:sd_hp) +
+#'                       (`Weight` = Mean:mean_wt + SD:sd_wt)))
+#'
+#' gt_tbl <- to_gt(tbl)
+#' print(gt_tbl)
+to_gt <- function(tbl,
+                  groupname_col = FALSE,
+                  ...){
+  if(!is.null(tbl$header$lhs)){
+    data_set <- cbind(tbl$table_data$row_data,
+                      tbl$table_data$col_data)
+  }else{
+    data_set <- tbl$table_data$col_data
+  }
+
+  gt_tbl <- gt::gt(data = data_set,
+                   groupname_col = groupname_col,
+                   ...)
+
+  # add the spanners
+  gt_tbl <- add_gt_spanners(gt_tbl = gt_tbl,
+                            tbl = tbl)
+
+  if(!is.null(tbl$header$lhs)){
+    rowname_headers <- colnames(tbl$table_data$row_data)
+    gt_tbl <- add_gt_rowname_separator(gt_tbl = gt_tbl,
+                                       right_of = rowname_headers[length(rowname_headers)])
+  }
+}
+
+#' flatten_table
+#'
+#' The table header within tables created with tablespan is represented in
+#' highly nested lists. The following function "flattens" this list to simplify
+#' implementing the same headers in gt.
+#' @param tbl table created with tablespan::tablespan
+#' @keywords internal
+#' @import gt
+#' @examples
+#' library(tablespan)
+#' library(dplyr)
+#' data("mtcars")
+#'
+#' summarized_table <- mtcars |>
+#'   group_by(cyl, vs) |>
+#'   summarise(N = n(),
+#'             mean_hp = mean(hp),
+#'             sd_hp = sd(hp),
+#'             mean_wt = mean(wt),
+#'             sd_wt = sd(wt))
+#'
+#' tbl <- tablespan(data = summarized_table,
+#'                  formula = (LHS = Cylinder:cyl + Engine:vs) ~
+#'                    N +
+#'                    (Results = (`Horse Power` = Mean:mean_hp + SD:sd_hp) +
+#'                       (`Weight` = Mean:mean_wt + SD:sd_wt)))
+#' str(tablespan:::flatten_table(tbl))
+flatten_table <- function(tbl){
+  if(!is.null(tbl$header$lhs)){
+    flattened_lhs <- flatten_table_partial(tbl_partial = tbl$header$lhs)
+  }else{
+    flattened_lhs <- NULL
+  }
+  flattened_rhs <- flatten_table_partial(tbl_partial = tbl$header$rhs)
+
+  return(list(flattened_lhs = flattened_lhs,
+              flattened_rhs = flattened_rhs))
+}
+
+#' flatten_table_partial
+#'
+#' Called by tablespan:::flatten_table. Recursive function that flattens the
+#' left hand or right hand side of the table headers.
+#' @param tbl_partial partial of a table header
+#' @param flattened list filled recursively
+#' @import gt
+#' @keywords internal
+#' @examples
+#' library(tablespan)
+#' library(dplyr)
+#' data("mtcars")
+#'
+#' summarized_table <- mtcars |>
+#'   group_by(cyl, vs) |>
+#'   summarise(N = n(),
+#'             mean_hp = mean(hp),
+#'             sd_hp = sd(hp),
+#'             mean_wt = mean(wt),
+#'             sd_wt = sd(wt))
+#'
+#' tbl <- tablespan(data = summarized_table,
+#'                  formula = (LHS = Cylinder:cyl + Engine:vs) ~
+#'                    N +
+#'                    (Results = (`Horse Power` = Mean:mean_hp + SD:sd_hp) +
+#'                       (`Weight` = Mean:mean_wt + SD:sd_wt)))
+#' str(tablespan:::flatten_table_partial(tbl$header$rhs))
+flatten_table_partial <- function(tbl_partial, flattened = list()){
+  if(!is.null(tbl_partial$entries)){
+    children <- list(list(level = tbl_partial$level,
+                          children = sapply(tbl_partial$entries,
+                                            function(x) x$name),
+                          # For items, tablespan can store a name that is different
+                          # from the actual item label to allow for renaming
+                          children_items = sapply(tbl_partial$entries,
+                                                  function(x) if(!is.null(x$item_name)) {x$item_name}else{x$name})))
+    names(children) <- tbl_partial$name
+    flattened <- c(flattened,
+                   children)
+    for(entry in tbl_partial$entries){
+      flattened <- flatten_table_partial(tbl_partial = entry,
+                                         flattened = flattened)
+    }
+  }
+  return(flattened)
+}
+
+#' add_gt_spanners
+#'
+#' Adds the spanners defined in a tablespan table to a gt table.
+#'
+#' @param gt_tbl gt table without spanners
+#' @param tbl table created with tablespan::tablespan
+#' @import gt
+#' @keywords internal
+add_gt_spanners <- function(gt_tbl, tbl){
+  flattened_tbl <- flatten_table(tbl)
+
+  if(!is.null(flattened_tbl$flattened_lhs)){
+    gt_tbl <- add_gt_spanner_partial(gt_tbl = gt_tbl,
+                                     tbl_partial = flattened_tbl$flattened_lhs)
+  }
+
+  gt_tbl <- add_gt_spanner_partial(gt_tbl = gt_tbl,
+                                   tbl_partial = flattened_tbl$flattened_rhs)
+
+  return(gt_tbl)
+}
+
+#' add_gt_spanner_partial
+#'
+#' Adds the spanners of the left hand side or right hand side of the headers
+#' defined in tablespan table to a gt table.
+#'
+#' @param gt_tbl gt table without spanners
+#' @param tbl_partial left or right hand side header of a table created with
+#' tablespan::tablespan
+#' @import gt
+#' @importFrom dplyr all_of
+#' @importFrom rlang :=
+#' @keywords internal
+add_gt_spanner_partial <- function(gt_tbl, tbl_partial){
+  # The table spanners need to be added in the correct order. All children of
+  # a spanner must already be in the table, otherwise we get an error.
+  # The level tells us the order; we have to start with the lowest one
+  levels <- sort(unique(sapply(tbl_partial, function(x) x$level)))
+
+  # Next, we iterate over the levels and add them to the gt:
+  for(level in levels){
+    for(parent_name in names(tbl_partial)){
+      if(parent_name == "_BASE_LEVEL_")
+        next
+      parent <- tbl_partial[[parent_name]]
+      if(parent$level == level){
+        item_names <- parent$children_items[parent$children_items %in% colnames(gt_tbl$`_data`)]
+        spanner_names <- parent$children_items[!parent$children_items %in% colnames(gt_tbl$`_data`)]
+
+        gt_tbl <- gt_tbl |>
+          gt::tab_spanner(label = parent_name,
+                          columns = dplyr::all_of(item_names),
+                          spanners = spanner_names)
+
+        # If children_items and children don't match, we also need to rename elements
+        to_rename <- which(parent$children_items != parent$children)
+
+        if(length(to_rename) > 0){
+          for(t_r in to_rename){
+            old_name <- parent$children_items[t_r]
+            gt_tbl <- gt_tbl |>
+              gt::cols_label({{old_name}} := parent$children[t_r])
+          }
+        }
+      }
+    }
+  }
+  return(gt_tbl)
+}
+
+#' add_gt_rowname_separator
+#'
+#' Adds a vertical line between the row names part and the data of the table.
+#' @param gt_tbl great table
+#' @param right_of name of the last data column to the right of which the actual
+#' data starts
+#' @import gt
+#' @keywords internal
+add_gt_rowname_separator <- function(gt_tbl,
+                                     right_of){
+  gt_tbl <- gt_tbl |>
+    gt::tab_style(style = gt::cell_borders(sides = c("right"),
+                                           weight = gt::px(2)),
+                  locations = gt::cells_body(columns = all_of(right_of)))
+  return(gt_tbl)
+}
